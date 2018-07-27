1db4934cd905164e7c83c8a126155d1a0b9d327b
Matic Zavadlal <matic.zavadlal@gmail.com>
2018-07-27 23:12:15 +0200

    course CRUD

--- server/src/resolvers/Mutation/course.ts
+++ server/src/resolvers/Mutation/course.ts
@@ -1,6 +1,33 @@
 import { Context } from '../../utils'
 
 export const course = {
+  async createCourse(parent, { name, lessons }, ctx: Context, info) {
+    return ctx.db.mutation.createCourse(
+      {
+        data: {
+          name,
+          lessons: {
+            connect: lessons.map(id => ({ id })),
+          },
+        },
+      },
+      info,
+    )
+  },
+  async updateCourse(parent, { id, name, lessons }, ctx: Context, info) {
+    return ctx.db.mutation.updateCourse(
+      {
+        where: { id },
+        data: {
+          name,
+          lessons: {
+            connect: lessons.map(id => ({ id })),
+          },
+        },
+      },
+      info,
+    )
+  },
   async deleteCourse(parent, { id }, ctx: Context, info) {
     return ctx.db.mutation.deleteCourse({ where: { id } }, info)
   },
